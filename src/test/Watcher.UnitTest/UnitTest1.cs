45bbe67b2cf269fefaa32b690a6fb851ed1cb9ca
Aaron Rinn <arinn1250@gmail.com>
2019-12-07 09:27:26 -0800

    Setup base test project

--- src/test/Watcher.UnitTest/UnitTest1.cs
+++ src/test/Watcher.UnitTest/UnitTest1.cs
@@ -1,12 +1,18 @@
+using AutoFixture;
+using AutoFixture.AutoMoq;
 using NUnit.Framework;
 
 namespace Watcher.UnitTest
 {
     public class Tests
     {
+        private Fixture _fixture;
+
         [SetUp]
         public void Setup()
         {
+            _fixture = new Fixture();
+            _fixture.Customize(new AutoMoqCustomization());
         }
 
         [Test]
